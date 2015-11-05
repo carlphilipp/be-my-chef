d710eaef86e76a8e52549200536bad7f97e0e465
Carl-Philipp Harmant <cp.harmant@gmail.com>
2015-11-04 22:57:02 -0600

    API-95 #time 1m Clean previous imports

--- src/test/java/com/epickur/api/dump/MongoDBDumpTest.java
+++ src/test/java/com/epickur/api/dump/MongoDBDumpTest.java
@@ -1,23 +1,25 @@
 package com.epickur.api.dump;
 
-import org.apache.commons.io.IOUtils;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class MongoDBDumpTest {
 
